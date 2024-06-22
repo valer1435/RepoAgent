94a9676ecfb8c408b6d799676787e6f7605d9cc9
valer1435 <valp998@gmail.com>
2024-06-22 09:39:15 +0300

    chaqnge prompt

--- repo_agent/prompt.py
+++ repo_agent/prompt.py
@@ -1,12 +1,12 @@
 SYS_PROMPT = """You are an AI documentation assistant, and your task is to generate documentation based on the given code of an object. The purpose of the documentation is to help developers and beginners understand the function and specific usage of the code.
 
-Currently, you are in a project{project_structure_prefix}
+Currently, you are in a project {project_structure_prefix}
 {project_structure}
 
 The path of the document you need to generate in this project is {file_path}.
 Now you need to generate a document for a {code_type_tell}, whose name is "{code_name}".
 
-The content of the code is as follows:
+The content of the code is as follows (Def means definition, just omit it):
 {code_content}
 
 {reference_letter}
@@ -18,14 +18,24 @@ Please write out the function of this {code_type_tell} in bold plain text, follo
 
 The standard format is as follows:
 
-**{code_name}**: The function of {code_name} is XXX. (Only code name and one sentence function description are required)
-**{parameters_or_attribute}**: The {parameters_or_attribute} of this {code_type_tell}.
+**{code_name}**
+The function of {code_name} is XXX. (Only code name and one sentence function description are required)
+
+**{parameters_or_attribute}**
+The {parameters_or_attribute} of this {code_type_tell}.
+
 · parameter1: XXX
+
 · parameter2: XXX
+
 · ...
-**Code Description**: The description of this {code_type_tell}.
+
+**Code Description**: 
+The description of this {code_type_tell}.
 (Detailed and CERTAIN code analysis and description...{has_relationship})
-**Note**: Points to note about the use of the code
+**Note**: 
+Points to note about the use of the code
+
 {have_return_tell}
 
 Please note:
