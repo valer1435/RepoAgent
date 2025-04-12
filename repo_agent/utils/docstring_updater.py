98f4c25c40050fddf9a9609d9678095edd847645
valer1435 <valp998@gmail.com>
2025-04-12 16:23:59 +0300

    fix indent

--- repo_agent/utils/docstring_updater.py
+++ repo_agent/utils/docstring_updater.py
@@ -21,9 +21,9 @@ Note:
     lines = new_docstring.split('\n')
     if len(lines) > 1:
         lines[1:] = [indent + line for line in lines[1:]]
-    processed_doc = '\n'.join(lines)
+    processed_doc = '\n'+indent+'\n'.join(lines)+'\n'
     if ast.get_docstring(node) is None:
         node.body.insert(0, ast.Expr(value=ast.Str(s=processed_doc)))
     else:
-        node.body[0].value.s = processed_doc
+        node.body[0] = ast.Expr(value=ast.Str(s=processed_doc))
     return node
